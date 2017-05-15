fe2530c22c922db2888eeec37e2ebc9c6d52d303
Etudiant <cedric.njonang@lepoles.com>
2017-05-15 11:52:19 +0200

    first commit

---
+++ Test/solution/TP1. Ex1.Parametre dans la tete.php
@@ -0,0 +1,13 @@
+<?php
+if ($_POST);
+if (condition) {
+    # code...
+}
+ ?>
+
+<form class="" action="index.html" method="post">
+    <label for=""></label>
+    <input type="text" name="" value="">
+
+    <button type="button" name="button">Envoye</button>
+</form>
